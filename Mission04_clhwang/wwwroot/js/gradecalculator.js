fc5a67ded9da3f52b287df551438553e9db750d2
crystalhwaang <chwang610@gmail.com>
2023-01-30 14:53:02 -0700

    new2

--- Mission04_clhwang/wwwroot/js/gradecalculator.js
+++ Mission04_clhwang/wwwroot/js/gradecalculator.js
@@ -1,1 +1,22 @@
-﻿$()
+﻿$("$btnSend").click(function () {
+    var assign = $('input[name = "assignments"]').val() * 0.50;
+    var quiz = $('input[name = "quizzes"]').val() * 0.10;
+    var project = $('input[name = "projects"]').val() * 0.10;
+    var intex = $('input[name = "quizzes"]').val() * 0.10;
+    var midterm = $('input[name = "midterm"]').val() * 0.10;
+    var final = $('input[name = "final"]').val() * 0.10;
+    var total = assign + quiz + project + intex + midterm + final
+    var output = 'A'
+
+    if (total >= 90) {
+        output = 'A'
+    } else if (total >= 80) {
+        output = 'B'
+    } else if (total >= 70) {
+        output = 'C'
+    } else if (total >= 60) {
+        output = 'D'
+    } else {
+        output = 'F'
+    }
+})
