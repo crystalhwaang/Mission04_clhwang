420d4f1937649b641cf48455385eeea2a4b5a637
crystalhwaang <chwang610@gmail.com>
2023-01-30 23:55:10 -0700

    i think i'm done?

--- Mission04_clhwang/wwwroot/js/gradecalculator.js
+++ Mission04_clhwang/wwwroot/js/gradecalculator.js
@@ -1,13 +1,17 @@
-﻿$(document).ready(function () {
+﻿// Creating the function that will be called in index.html to calculate the grades
+$(document).ready(function () {
     $("#btnCalculate").click(function () {
+        // Creating variables for all of the different inputs and calculating each of their individual percentages
         var assign = $("#assignments").val() * 0.50;
         var quiz = $("#quizzes").val() * 0.10;
         var project = $("#projects").val() * 0.10;
         var intex = $("#intex").val() * 0.10;
         var midterm = $("#midterm").val() * 0.10;
         var final = $("#final").val() * 0.10;
+        // Calculating their total percentage
         var total = assign + quiz + project + intex + midterm + final;
-        var output = 'hi';
+        var output = ' ';
+        // Determining their letter grade based on their percentage
         if (total >= 90) {
             output = 'A';
         } else if (total >= 80) {
@@ -19,6 +23,7 @@
         } else {
             output = 'F';
         }
+        //Assigning an ID so that this function can be called in index.html
         $("#print").text(output);
     });
 });
