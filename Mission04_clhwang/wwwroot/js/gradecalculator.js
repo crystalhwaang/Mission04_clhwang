01da24dc046abb451a7dbe249b8ef266464d44da
crystalhwaang <chwang610@gmail.com>
2023-01-30 23:16:30 -0700

    yay!

--- Mission04_clhwang/wwwroot/js/gradecalculator.js
+++ Mission04_clhwang/wwwroot/js/gradecalculator.js
@@ -1,22 +1,24 @@
-﻿$("#btnCalculate").click(function() {
-    var assign = $('input[name = "assignments"]').val() * 0.50;
-    var quiz = $('input[name = "quizzes"]').val() * 0.10;
-    var project = $('input[name = "projects"]').val() * 0.10;
-    var intex = $('input[name = "quizzes"]').val() * 0.10;
-    var midterm = $('input[name = "midterm"]').val() * 0.10;
-    var final = $('input[name = "final"]').val() * 0.10;
-    var total = assign + quiz + project + intex + midterm + final;
-    var output = 'hi';
-    if (total >= 90) {
-        output = 'A';
-    } else if (total >= 80) {
-        output = 'B';
-    } else if (total >= 70) {
-        output = 'C';
-    } else if (total >= 60) {
-        output = 'D';
-    } else {
-        output = 'F';
-    }
-    document.getElementById("print").value = output;
-})
+﻿$(document).ready(function () {
+    $("#btnCalculate").click(function () {
+        var assign = $("#assignments").val() * 0.50;
+        var quiz = $("#quizzes").val() * 0.10;
+        var project = $("#projects").val() * 0.10;
+        var intex = $("#intex").val() * 0.10;
+        var midterm = $("#midterm").val() * 0.10;
+        var final = $("#final").val() * 0.10;
+        var total = assign + quiz + project + intex + midterm + final;
+        var output = 'hi';
+        if (total >= 90) {
+            output = 'A';
+        } else if (total >= 80) {
+            output = 'B';
+        } else if (total >= 70) {
+            output = 'C';
+        } else if (total >= 60) {
+            output = 'D';
+        } else {
+            output = 'F';
+        }
+        $("#print").text(output);
+    });
+});
