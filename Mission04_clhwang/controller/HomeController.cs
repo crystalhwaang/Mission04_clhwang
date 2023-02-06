d7f3f9b9049baa736da33bd52913443f6fe8975a
crystalhwaang <chwang610@gmail.com>
2023-02-06 16:25:01 -0700

    new

---
+++ Mission04_clhwang/controller/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission04_clhwang.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_clhwang.controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission04_clhwang/models/GradeCalculatorModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_clhwang.models
+{
+    public class GradeCalculatorModel
+    {
+        [Required]
+        public int Assignments { get; set; }
+        public int Quizzes { get; set; }
+        public int Projects { get; set; }
+        public int Intex { get; set; }
+        public int Midterm { get; set; }
+        public int Final { get; set; }
+
+
+    }
+}
